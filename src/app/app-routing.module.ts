ef4330bb484b2b72489feb2d9214516b3c93995d
uparkar <uparkar@eins.de>
2023-10-01 22:04:22 +0530

    MA-1 | Update introduction routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'introduction',
-    loadChildren: () => import('./introduction/introduction.module').then( m => m.IntroductionPageModule)
+    loadChildren: () => import('./widgets/content/introduction/introduction.module').then( m => m.IntroductionPageModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./widgets/content/introduction/introduction.module').then( m => m.IntroductionPageModule)
   },
 ];
 
